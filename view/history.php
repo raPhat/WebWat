cf625dd8513fbffacd2dd7e40c9866003d456468
auttapon nantachinda <auttapon_n@cmu.ac.th>
2015-10-17 23:08:30 +0700

    เพิ่มเมนู

---
+++ view/history.php
@@ -0,0 +1,7 @@
+<?php require_once( ROOT_VIEW . 'header.php' ); ?>
+
+    <div id="sub-container" class="sub-container">
+      test
+    </div>
+
+<?php require_once( ROOT_VIEW . 'footer.php' ); ?>
